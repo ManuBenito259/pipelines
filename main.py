89313488598f44c4841a071369f71f5896a55463
ManuBenito <manuelbenitobarahona@gmail.com>
2023-03-02 15:50:39 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Este módulo contiene el script principal del proyecto.
+"""
+
+import os
+import subprocess
+
+
+def main():
+    """Este es el docstring de mi_paquete"""
+    # Crear directorio 'build' si no existe
+    os.makedirs('build', exist_ok=True)
+
+    # Compilar archivo index.html
+    subprocess.run(['touch', 'build/index.html'], check=False)
+
+
+if __name__ == '__main__':
+    main()
